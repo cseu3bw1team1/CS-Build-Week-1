7922f50e0a2bef5e487f9026587038efe6013d78
Brenda Joshua <brendajosh.bj@gmail.com>
2020-03-04 11:05:33 +0300

    add end points

--- adventure/urls.py
+++ adventure/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
+    url('room', api.room),
+    url('get', api.get),
+    url('drop', api.drop),
+    url('look', api.look),
+    url('inventory', api.inventory),
 ]
